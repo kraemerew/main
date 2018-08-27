378ec70e5a6d103073fcaa30ce028d5d9cc11e53
Developer <kraemer@subsymbolics.com>
2018-08-27 20:51:54 +0200

    Cleanup

--- cam/framereader.cpp
+++ cam/framereader.cpp
@@ -54,23 +54,14 @@ public:
 private:
     QRgb yuv2rgb(int y, int u, int v) const
     {
-       int r1, g1, b1;
-       int c = y-16, d = u - 128, e = v - 128;
+       const int c = y-16, d = u - 128, e = v - 128,
 
-       r1 = (298 * c           + 409 * e + 128) >> 8;
-       g1 = (298 * c - 100 * d - 208 * e + 128) >> 8;
-       b1 = (298 * c + 516 * d           + 128) >> 8;
+       r = (298 * c           + 409 * e + 128) >> 8,
+       g = (298 * c - 100 * d - 208 * e + 128) >> 8,
+       b = (298 * c + 516 * d           + 128) >> 8;
 
        // Even with proper conversion, some values still need clipping.
-
-       if (r1 > 255) r1 = 255;
-       if (g1 > 255) g1 = 255;
-       if (b1 > 255) b1 = 255;
-       if (r1 < 0) r1 = 0;
-       if (g1 < 0) g1 = 0;
-       if (b1 < 0) b1 = 0;
-
-       return qRgb(r1,g1,b1);
+       return qRgb(qBound(0,r,255),qBound(0,g,255),qBound(0,b,255));
     }
 };
 
