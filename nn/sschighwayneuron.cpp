c1098ced1fe420916ef2a2cf17da3599002778ef
Developer <kraemer@subsymbolics.com>
2019-06-25 20:21:53 +0200

    More work on highway

--- nn/sschighwayneuron.cpp
+++ nn/sschighwayneuron.cpp
@@ -6,63 +6,18 @@ SScHighwayNeuron::SScHighwayNeuron(SScHighwayNetwork* nw, int layer, int nr)
     : SScNeuron (SScNeuron::NeuronType_Hidden),
       m_nw      (nw),
       m_layer   (layer),
-      m_nr      (nr)
+      m_nr      (nr),
+      m_in      (this),
+      m_cin     (this),
+      m_act     (SScActivation::create(SScActivation::ACT_TANH)),
+      m_cact    (SScActivation::create(SScActivation::ACT_SIGMOID)),
+      m_hw      (NULL)
 {
     Q_CHECK_PTR(nw);
 }
 
-
-double SScHighwayNeuron::out()
+SScHighwayNeuron::~SScHighwayNeuron()
 {
-    const double tout  = m_nw->transformNeuron(m_layer)->out(),
-                 carry = 1.0-tout;
-    return tout *m_act->activate(net())
-          +carry*m_nw->neuron(m_layer-1,m_nr)->out();
-}
-
-bool SScHighwayNeuron::addInput(SScNeuron *other, double v)
-{
-    Q_CHECK_PTR(other);
-    if ((this==other) || m_in.contains(other)) return false;
-    m_in[other]=QSharedPointer<SScTrainableParameter>(SScTrainableParameter::create(SScTrainableParameter::CON_RPROP,v));
-    return true;
-}
-
-bool SScHighwayNeuron::delInput(SScNeuron *other)
-{
-    Q_CHECK_PTR(other);
-    if (!m_in.contains(other)) return false;
-    m_in.remove(other);
-    return true;
-}
-
-double SScHighwayNeuron::net()
-{
-    double ret = 0;
-    for(QMap<SScNeuron*,QSharedPointer<SScTrainableParameter> >::iterator it = m_in.begin(); it != m_in.end(); ++it)
-        ret += it.key()->out()*it.value()->value();
-    return ret;
-}
-
-double SScHighwayNeuron::deltag()
-{
-    return -dedo()*net()*m_act->dev();
-}
-
-double SScHighwayNeuron::deltaw(SScNeuron* n)
-{
-    //TODO
-    // dE/dw_ij=dE/doj*doj/dw_ij=dedo()*doj/dnetj*dnetj/dwij
-    // = dedo()*doj/dnetj*oi
-
-    return -dedo()*n->out()*m_act->dev()*m_act->gain();
-}
-
-
-bool SScHighwayNeuron::trainingStep(bool cycleDone)
-{
-    for(QMap<SScNeuron*,QSharedPointer<SScTrainableParameter> >::iterator it = m_in.begin(); it != m_in.end(); ++it)
-        it.value()->update(deltaw(it.key()),cycleDone);
-    m_act->updateGain(deltag(),cycleDone);
-    return true;
+    delete m_act;
+    delete m_cact;
 }
--- nn/sschighwayneuron.hpp
+++ nn/sschighwayneuron.hpp
@@ -2,6 +2,9 @@
 #define HWNEURON_HPP
 
 #include "sscneuron.hpp"
+#include "sscgate.hpp"
+#include "sscactivation.hpp"
+
 #include <QMap>
 #include <QSharedPointer>
 
@@ -17,25 +20,39 @@ public:
      * \param nr    Index in layer
      */
     explicit SScHighwayNeuron(SScHighwayNetwork* nw, int layer, int nr);
+    virtual ~SScHighwayNeuron();
 
-    virtual bool  setInput(double) { Q_ASSERT(false); return false; }
-    virtual bool  setTarget(double) { Q_ASSERT(false); return false; }
+    virtual bool    setInput(double) { Q_ASSERT(false); return false; }
+    virtual bool    setTarget(double) { Q_ASSERT(false); return false; }
+    virtual void    reset() {}
+    virtual bool    addInput(SScNeuron* other, double v) { return m_in.addInput(other,v); }
+    virtual bool    delInput(SScNeuron* other) { return m_in.delInput(other); }
+    virtual bool    addInputC(SScNeuron* other, double v) { return m_cin.addInput(other,v); }
+    virtual bool    delInputC(SScNeuron* other) { return m_cin.delInput(other); }
+    virtual double  net() { return m_in.net(); }
+    virtual double  netC() { return m_cin.net(); }
 
-    virtual bool addInput(SScNeuron* other, double v);
-    virtual bool delInput(SScNeuron* other);
-    virtual double net();
-    virtual double deltaw(SScNeuron*);
-    virtual double deltag();
-    virtual double out();
+    virtual double deltaw(SScNeuron*) { return 0; } //TODO
+    virtual double deltag() { return 0; } // TODO
+    virtual double deltawC(SScNeuron*) { return 0; } //TODO
+    virtual double deltagC() { return 0; } // TODO
+
+    virtual double out() { const double c = outC(); return m_act->activate(net())*(1.0-c)+c*highway(); }
+    virtual double outC() { return m_cact->activate(netC()); }
 
     virtual QList<SScNeuron*> inputs() const { return m_in.keys(); }
+    virtual QList<SScNeuron*> inputsC() const { return m_cin.keys(); }
 
-    virtual bool trainingStep(bool cycleDone);
+    virtual bool trainingStep(bool cycleDone) { return false; } //TODO
+    virtual double highway() { return m_hw ? m_hw->out() : 0.0; }
+    virtual void setHighway(SScNeuron* hw) { m_hw=hw; }
 
 protected:
     SScHighwayNetwork* m_nw;
-    int m_layer, m_nr;
-    QMap<SScNeuron*,QSharedPointer<SScTrainableParameter> > m_in;
-
+    int             m_layer, m_nr;
+    SScGate         m_in, m_cin;
+    SScActivation*  m_act;
+    SScActivation*  m_cact;
+    SScNeuron*      m_hw;
 };
 #endif // HWNEURON_HPP
