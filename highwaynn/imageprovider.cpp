d71719cedc76b03310ee7ae6cf904b51a6b8b5e6
Developer <kraemer@subsymbolics.com>
2019-12-04 19:28:55 +0100

    SScImageProviderV2

--- highwaynn/imageprovider.cpp
+++ highwaynn/imageprovider.cpp
@@ -1,6 +1,105 @@
 #include "imageprovider.hpp"
 #include "image/convimageconverter.hpp"
 #include <QUuid>
+#include "convhelpers.hpp"
+QString SScImageProviderV2::append(const QString& file)
+{
+    return append(QImage(file));
+}
+QString SScImageProviderV2::append(const QImage& im)
+{
+    if (!im.isNull())
+    {
+        m_seq << QUuid::createUuid().toString();
+        m_data[m_seq.last()]=im;
+        m_current = m_seq.last();
+        m_last = m_seq.last();
+        return m_current;
+    }
+    return QString();
+}
+QString SScImageProviderV2::next(bool& endOfCycle)
+{
+    endOfCycle=false;
+    if (!m_seq.isEmpty())
+    {
+        m_current = m_seq.takeFirst();
+        m_seq.append(m_current);
+        endOfCycle = (m_current==m_last);
+        return m_current;
+    }
+    return QString();
+}
+QString SScImageProviderV2::prev(bool& endOfCycle)
+{
+    endOfCycle=false;
+    if (!m_seq.isEmpty())
+    {
+        m_current = m_seq.takeLast();
+        m_seq.prepend(m_current);
+        endOfCycle = (m_current==m_last);
+        return m_current;
+    }
+    return QString();
+}
+bool SScImageProviderV2::select(const QString &uuid)
+{
+    if (!m_data.contains(uuid)) return false;
+    const QString backup = m_current;
+    bool dummy;
+    int ctr = 0;
+    while ((m_current!=uuid) && (++ctr<=m_seq.size())) next(dummy);
+    if (m_current!=uuid) m_current=backup;
+    return (m_current==uuid);
+}
+bool SScImageProviderV2::first()
+{
+    if (m_data.isEmpty()) return false;
+    if (!last()) return false;
+    bool dummy;
+    next(dummy);
+    return true;
+}
+bool SScImageProviderV2::last()
+{
+    if (m_data.isEmpty()) return false;
+    bool dummy;
+    while (m_current!=m_last) next(dummy);
+    return true;
+}
+
+QVector<double> SScImageProviderV2::get(const QSize& kernel, const QSize& stride, const QSize& elements, bool color)
+{
+    const QString id = calcId(m_current,kernel,stride,elements,color);
+    if (!m_cached.contains(id))
+    {
+        const auto im = image(SSnConvHelper::inputSize(kernel,stride,elements));
+        if (!im.isNull())
+        {
+            QVector<double> data;
+            const auto m = color ? SSnConvHelper::colorValues(SSnConvHelper::matrix(im,kernel,stride,elements))
+                                 : SSnConvHelper::grayValues (SSnConvHelper::matrix(im,kernel,stride,elements));
+            foreach(auto cline, m) foreach(auto c, cline)
+            {
+                double v = c;
+                v=(v-128.0)/128.0;
+                data << v;
+            }
+            m_cached[id]=data;
+        }
+    }
+    return m_cached.contains(id) ? m_cached[id] : QVector<double>();
+}
+
+QString SScImageProviderV2::calcId(const QString& uuid, const QSize &kernel, const QSize &stride, const QSize &elements, bool color) const
+{
+    const QStringList sl = QStringList() << uuid
+                                   << QString("%1").arg(kernel.width()) << QString("%1").arg(kernel.height())
+                                   << QString("%1").arg(stride.width()) << QString("%1").arg(stride.height())
+                                   << QString("%1").arg(elements.width()) << QString("%1").arg(elements.height())
+                                   << (color ? "C":"M");
+    return sl.join(" ");
+}
 
 SScImageProvider::SScImageProvider(int kx, int ky, int overlap, int outw, int outh, bool color)
   : m_kx    (kx),
--- highwaynn/imageprovider.hpp
+++ highwaynn/imageprovider.hpp
@@ -2,9 +2,84 @@
 #define IMAGEPROVIDER_HPP
 
 #include <QMap>
+#include <QHash>
 #include <QStringList>
 #include <QVector>
 #include <QImage>
+
+class SScImageProviderV2
+{
+public:
+    /*!
+     * \brief Load and append image
+     * \param file Filename
+     * \return unique id for this pattern - empty on failure
+     */
+    QString append(const QString& file);
+    /*!
+     * \brief Append image
+     * \param im
+     * \return unique id for this pattern - empty on failure
+     */
+    QString append(const QImage& im);
+    /*!
+     * \brief Select next pattern
+     * \param endOfCycle    Will be true if end of cycled is reached (used to determine batch end)
+     * \return Current uuid
+     */
+    QString next(bool& endOfCycle);
+    /*!
+     * \brief Select previous pattern
+     * \param endOfCycle    Will be true if end of cycled is reached (used to determine batch end)
+     * \return Current uuid
+     */
+    QString prev(bool& endOfCycle);
+    /*!
+     * \brief Select pattern by uuid
+     * \param uuid
+     * \return false on failure
+     */
+    bool select(const QString& uuid);
+    /*!
+     * \brief Select first pattern
+     * \return
+     */
+    bool first();
+    /*!
+     * \brief Select last pattern
+     * \return
+     */
+    bool last();
+    /*!
+     * \brief Return whether a pattern is activated
+     * \return
+     */
+    inline bool isActive() const { return !m_current.isEmpty(); }
+    /*!
+     * \brief Return current selected image
+     * \return
+     */
+    inline QImage image() const { return m_data.contains(m_current) ? m_data[m_current]  : QImage(); }
+
+    /*!
+     * \brief Return image preprocessed for convolution layer
+     * \param kernel    Kernel size
+     * \param stride    Kernel stride
+     * \param elements  Number of output elements
+     * \param color     Color data or grayvalue data
+     * \return
+     */
+    QVector<double> get(const QSize& kernel, const QSize& stride, const QSize& elements, bool color);
+
+private:
+    QString calcId(const QString& uuid, const QSize& kernel, const QSize& stride, const QSize& elements, bool color) const;
+    inline QImage image(const QSize& sz) const { return m_data.contains(m_current) ? m_data[m_current].scaled(sz,Qt::IgnoreAspectRatio,Qt::SmoothTransformation)  : QImage(); }
+
+    QHash<QString,QImage>           m_data;
+    QHash<QString,QVector<double> > m_cached;
+    QStringList                     m_seq;
+    QString                         m_current, m_last;
+};
 
 class SScImageProvider
 {
